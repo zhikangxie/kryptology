813add0ef50fcb2b1936a8b83e67a3e6bb75a2de
zhikangxie <zkxiecs@gmail.com>
2023-05-18 19:14:44 +0800

    draft of dkg

--- pkg/tsm2/cetsm2/dkg/dkg.go
+++ pkg/tsm2/cetsm2/dkg/dkg.go
@@ -3,32 +3,90 @@ package dkg
 import (
 	"crypto/rand"
 	"github.com/coinbase/kryptology/pkg/core/curves"
+	"github.com/coinbase/kryptology/pkg/ot/base/simplest"
+	"github.com/coinbase/kryptology/pkg/zkp/chaumpedersen"
 	"github.com/coinbase/kryptology/pkg/zkp/schnorr"
 )
 
 type Party struct {
-	prover *schnorr.Prover
-	proof  *schnorr.Proof
-	curve  *curves.Curve
-	sk     curves.Scalar
-	pk     curves.Point
+	n  int
+	id int
+
+	curve     *curves.Curve
+	basePoint curves.Point
+
+	sk       curves.Scalar
+	pk       curves.Point
+	pkProver *schnorr.Prover
+	pkProof  *schnorr.Proof
+
+	pkJointMidProver *chaumpedersen.Prover
+
+	pkJoint curves.Point
+
+	peerCommitment       []schnorr.Commitment
+	peerPk               []curves.Point
+	peerPkProof          []*schnorr.Proof
+	peerPkProofSessionId [][]byte
+
+	pkJointMid               []curves.Point
+	pkJointMidProof          []*chaumpedersen.Proof
+	pkJointMidProofSessionId [][]byte
 }
 
-func NewParty(curve *curves.Curve) *Party {
+func NewParty(n int, id int, curve *curves.Curve, basePoint curves.Point) *Party {
 	return &Party{
-		curve: curve,
+		n:         n,
+		id:        id,
+		curve:     curve,
+		basePoint: basePoint,
 	}
 }
 
-func (party *Party) ComProve() (schnorr.Commitment, error) {
+// functions for step 1
+
+func (party *Party) KeyGen() {
 	party.sk = party.curve.Scalar.Random(rand.Reader)
 	party.pk = party.curve.ScalarBaseMult(party.sk)
+}
 
-	proof, commitment, err := party.prover.ProveCommit(party.sk)
+func (party *Party) PkComProve() (schnorr.Commitment, error) {
+	uniqueSessionId := [simplest.DigestSize]byte{}
+	party.pkProver = schnorr.NewProver(party.curve, party.basePoint, uniqueSessionId[:])
+	dlProof, commitment, err := party.pkProver.ProveCommit(party.sk)
 	if err != nil {
 		return nil, err
 	}
-	party.proof = proof
+	party.pkProof = dlProof
 
 	return commitment, nil
+}
+
+// functions for step 2
+
+func (party *Party) PeerComReceive(commitment schnorr.Commitment, peerNum int) {
+	party.peerCommitment[peerNum-1] = commitment
+}
+
+func (party *Party) PeerDeComReceive(pk curves.Point, proof *schnorr.Proof, uniqueSessionId []byte, peerNum int) {
+	party.peerPk[peerNum-1] = pk
+	party.peerPkProof[peerNum-1] = proof
+	party.peerPkProofSessionId[peerNum-1] = uniqueSessionId
+}
+
+func (party *Party) PeerPksVerify() error {
+	var err error
+	for i := 0; i <= party.n-1; i++ {
+		err = schnorr.DecommitVerify(party.peerPkProof[i], party.peerCommitment[i], party.curve, party.basePoint, party.peerPkProofSessionId[i])
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// functions for step 3
+
+func (party *Party) PkJointMidComp() {
+
 }
